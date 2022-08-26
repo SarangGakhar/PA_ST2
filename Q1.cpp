332cf6de01e18198b2c9e0dc184e77161c24150c
sarang <gakharsarang1@gmail.com>
2022-08-26 14:46:28 +0530

    q1

--- Q1.cpp
+++ Q1.cpp
@@ -1,7 +1,17 @@
 /* 
 sarang 
 2010991713
-set-
+set-04
 Question 1
  */
 
+
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+
+int main(){
+ios_base::sync_with_stdio(false); cin.tie(NULL);
+return 0;
+}
